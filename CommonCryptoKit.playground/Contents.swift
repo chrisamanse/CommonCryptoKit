204eb6ddbf86231be268a52a8beb8ffdeaa693f7
Chris Amanse <chris@chrisamanse.xyz>
2016-08-14 20:25:59 +0800

    Test CommonCrypto in Swift

--- CommonCryptoKit.playground/Contents.swift
+++ CommonCryptoKit.playground/Contents.swift
@@ -1,6 +1,20 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
+import Foundation
 import CommonCryptoKit
 
-var str = "Hello, playground"
+var str = "https://www.google.com/logos/doodles/2016/parents-day-in-korea-5757703554072576-hp2x.jpg"
+
+let key = "google".data(using: .utf8)!.map { $0 }
+let data = str.data(using: .utf8)!.map { $0 }
+
+let hashAlgo = CCHmacAlgorithm(kCCHmacAlgSHA1)
+let hashLength = Int(CC_SHA1_DIGEST_LENGTH)
+
+var result = Array(repeating: UInt8(0x00), count: hashLength)
+
+CCHmac(hashAlgo, key, key.count, data, data.count, &result)
+
+result
+
+let resultData = Data(bytes: result)
